8401c9945da547e2f0324c0e4fa2640e831a4984
Marina Utkina <marryx@inbox.ru>
2023-03-14 02:47:19 +0300

    Внесены исправлению. Ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,11 @@ const profileConteiner = document.querySelector('.profile');
 const popupForm = document.querySelector('.popup');
 const popupOpenButtonForm = profileConteiner.querySelector('.profile__edit-button');
 const popupCloseButtonForm = popupForm.querySelector('.popup__close-icon');
-
+let nameProfile = profileConteiner.querySelector('.profile__name');
+let jobProfile = profileConteiner.querySelector('.profile__job');
+let nameInput = popupForm.querySelector('.popup__item_el_name');
+let jobInput = popupForm.querySelector('.popup__item_el_job');
+const popupButtonSubmit = popupForm.querySelector('.popup__button-submit');
 
 const openPopupForm = function() {
   popupForm.classList.add('popup_opened');
@@ -12,14 +16,12 @@ const closePopupForm = function() {
   popupForm.classList.remove('popup_opened');
 }
 
-popupOpenButtonForm.addEventListener('click', openPopupForm);
+popupOpenButtonForm.addEventListener('click', function() {
+  openPopupForm();
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+});
 popupCloseButtonForm.addEventListener('click', closePopupForm);
-
-let nameProfile = profileConteiner.querySelector('.profile__name');
-let jobProfile = profileConteiner.querySelector('.profile__job');
-let nameInput = popupForm.querySelector('.popup__name');
-let jobInput = popupForm.querySelector('.popup__job');
-const popupButtonSubmit = popupForm.querySelector('.popup__button-submit');
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
